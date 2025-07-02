c5d7bbbc342228bfee3a9513e8a6f61f7e98fd68
Geun100 <kdg25800@naver.com>
2025-07-02 20:25:34 +0900

    JavaScript-handling events

--- Javascript 쇼핑몰 미니게임 과제/src/main.js
+++ Javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -20,6 +20,25 @@ function displayItems(items) {
       `;
   }
 
+  function onButtonClick(event, items) {
+  const dataset = event.target.dataset;
+  const key = dataset.key;
+  const value = dataset.value;
+
+  if (key == null || value == null) {
+    return;
+  }
+
+  displayItems(items.filter(item => item[key] === value));
+}
+
+function setEventListeners(items) {
+  const logo = document.querySelector('.logo');
+  const buttons = document.querySelector('.buttons');
+  logo.addEventListener('click', () => displayItems(items));
+  buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then(items => {
