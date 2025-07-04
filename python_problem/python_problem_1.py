205164e2e785dc09544f7971624805fd9be81de4
Geun100 <kdg25800@naver.com>
2025-07-04 18:55:19 +0900

    8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,30 +1,33 @@
-num = 0
-current = 1
-turn = 0  # 0: playerA, 1: playerB
+def brGame():
+    num = 0
+    current = 1
+    turn = 0  # 0: playerA, 1: playerB
 
-while current <= 31:
-    while True:
-        try:
-            num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
-            if num not in [1, 2, 3]:
-                print("1,2,3 중 하나를 입력하세요")
-                continue
-            break
-        except ValueError:
-            print("정수를 입력하세요")
-    for i in range(num):
+    while current <= 31:
+        while True:
+            try:
+                num = int(input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : "))
+                if num not in [1, 2, 3]:
+                    print("1,2,3 중 하나를 입력하세요")
+                    continue
+                break
+            except ValueError:
+                print("정수를 입력하세요")
+        for i in range(num):
+            if current > 31:
+                break
+            if turn == 0:
+                print(f"playerA : {current}")
+            else:
+                print(f"playerB : {current}")
+            current += 1
         if current > 31:
             break
-        if turn == 0:
-            print(f"playerA : {current}")
-        else:
-            print(f"playerB : {current}")
-        current += 1
-    if current > 31:
-        break
-    turn = 1 - turn
+        turn = 1 - turn
 
-if turn == 1:
-    print("playerA win!")
-else:
-    print("playerB win!")
+    if turn == 1:
+        print("playerA win!")
+    else:
+        print("playerB win!")
+
+brGame()
